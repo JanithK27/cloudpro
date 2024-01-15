13b0548d7b4935b2d86d9396444ce704879451f6
Janith Hettiarachchi <93459203+JanithK27@users.noreply.github.com>
2024-01-16 00:20:45 +0530

    Update api_gateway.tf

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,3 +1,4 @@
+
 resource "aws_api_gateway_rest_api" "my_api" {
   name        = "MyAPI"
   description = "My API"
@@ -43,35 +44,6 @@ resource "aws_api_gateway_deployment" "my_api_deployment" {
   lifecycle {
     create_before_destroy = true
   }
-
-  instances = [
-    {
-      schema_version = 0
-      attributes     = {
-        created_date    = "2023-12-08T23:07:58Z",
-        description     = "",
-        execution_arn    = "arn:aws:execute-api:us-east-1:255945442255:uzcqojsnob/dev",
-        id              = "2v861d",
-        invoke_url      = "http://3.0.189.27/wordpress/",  # Updated URL
-        rest_api_id     = aws_api_gateway_rest_api.my_api.id,
-        stage_description = null,
-        stage_name      = "dev",
-        triggers        = null,
-        variables       = null
-      }
-      sensitive_attributes = []
-      private              = "bnVsbA=="
-      dependencies         = [
-        "aws_api_gateway_integration.lambda_integration",
-        "aws_api_gateway_method.api_method",
-        "aws_api_gateway_resource.api_resource",
-        "aws_api_gateway_rest_api.my_api",
-        "aws_iam_role.lambda_exec",
-        "aws_lambda_function.my_lambda",
-      ]
-      create_before_destroy = true
-    },
-  ]
 }
 
 data "aws_region" "current" {}
